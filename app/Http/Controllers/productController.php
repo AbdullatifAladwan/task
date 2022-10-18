bc7bb2e212f51e4923c50770e41e4d608e5e8c97
Abdullatif Aladwan <abdullatifaladwan@gmial.com>
2022-10-18 14:13:49 +0300

    final

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -9,6 +9,7 @@ class productController extends Controller
 {
     public function index()
     {
+        //show all product from database
         $product = product::all();
         return view('index', compact('product'));
     }
@@ -16,12 +17,14 @@ class productController extends Controller
 
     public function create()
     {
+        //show form to add prodact to database
         return view('create');
     }
 
 
     public function store(Request $request)
     {
+        //function to save submit form to database
         $data = new product();
         $request->validate([
             'name' => 'required',
@@ -49,12 +52,15 @@ class productController extends Controller
 
     public function show(product $product)
     {
+        //show info for prodact
+
         return view('show', compact('product'));
     }
 
 
     public function edit(product $product)
     {
+        //show edit product from  
         return view('edit', compact('product'));
     }
 
@@ -63,6 +69,7 @@ class productController extends Controller
     public function update(Request $request, product $product)
     {
 
+        //function to save submit form edit to database
 
         $input = $request->all();
         if ($image = $request->file('img')) {
@@ -84,9 +91,9 @@ class productController extends Controller
 
     public function destroy($id)
     {
-
+        //delet product 
         Product::find($id)->delete($id);
-
+        //ajax
         return response()->json([
             'success' => 'Product deleted successfully!'
         ]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,8 @@
 @extends('layout')
      
 @section('content')
-
-
-
-
 <script>
     alert('please press task emperorsoft')
 </script>
-
 @endsection
+
